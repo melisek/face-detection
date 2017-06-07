7e07aa14a99be515a57443c656251a523f502de8
melisek <mark.melisek@gmail.com>
2017-06-07 22:12:51 +0200

    Init

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var multer  = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+var app = express();
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Load credentials and set region from JSON file
+AWS.config.loadFromPath('./aws-config.json');
+
+var rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
+var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+app.use(bodyParser.json({limit: '5mb'})); // Amazon Rekognition limits image size at 5MB
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.static('public')); // serving static files
+
+// Handle POST request 
+// Copy captured image blob to memoryStorage
+app.post('/recognize', upload.single('img'), function (req, res, next) {
+	
+	var params = {
+		"Image": {
+			"Bytes": req.file.buffer // Retrieving image Buffer from memoryStorage
+		},
+		"Attributes": ["ALL"] // Get all facial attributes from Rekognition
+	};
+
+	rekognition.detectFaces(params, function(err, data) {
+		if (err) {
+			console.log(err, err.stack); // an error occurred
+			res.status(500).send(err)
+		}
+		else {
+			var str = JSON.stringify(data, null, '\t');
+			console.log(str); // successful response
+			return res.send(str);
+		}
+   });
+});
+ 
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
---
+++ public/js/main.js
@@ -0,0 +1,108 @@
+'use strict';
+
+// Using Realeyesit Environment Checker for webcam and browser compatibility 
+
+var notCapableErrorCodes = [4, 5, 6, 7]; // excluding flash
+
+function environmentalDetectionCallback(result) {
+	if (notCapableErrorCodes.indexOf(result.failureReasonCode) != -1)	{
+		$('#non-capable').show();
+		$('#capable').hide();
+		console.log('Environment non-capable: ' + result.failureReasonString);
+	}
+	else
+		$('#non-capable').hide();
+}
+
+var _RealeyesitEnvDetectParams = _RealeyesitEnvDetectParams || {};
+_RealeyesitEnvDetectParams._callback = environmentalDetectionCallback;
+
+// Using WebRTC adapter.js for streaming webcam to video element
+
+var video = document.getElementById('video');
+
+var constraints = {
+	audio: false,
+	video: true
+};
+
+function handleSuccess(stream) {
+	video.srcObject = stream;
+	$('#camera-allowed').show();
+}
+
+function handleError(error) {
+	$('#camera-denied').show();
+	console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+
+// Draw captured image to canvas and post image blob to server for face recognition
+
+var canvas = window.canvas = document.getElementById('canvas');
+var button = $('#captureBtn');
+
+button.click(function() {
+  canvas.width = video.width;
+  canvas.height = video.height;
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+  getCanvasBlob();
+});
+	
+function getCanvasBlob() {
+	canvas.toBlob(function (blob) {
+		var data = new FormData();
+		data.append('img', blob);
+
+		$.ajax({
+			url :  "./recognize",
+			type: 'POST',
+			data: data,
+			contentType: false,
+			processData: false,
+			success: function(data) { 
+				var json = JSON.parse(data);
+				console.log(json);
+				displayResult(json);
+			},    
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	});
+}
+
+// Display results from Amazon Rekognition
+function displayResult(json) {
+	$('#analysis').html('<p>Faces recognized: ' + json.FaceDetails.length + '</p>');
+	
+	var strokeColor = [ 255, 255, 255 ];
+	$.each( json.FaceDetails, function( i, face ) {
+		// display faces only over 50 confidence level
+		if (face.Confidence > 50) {
+			
+			var box = face.BoundingBox;
+			var ctx = canvas.getContext("2d");
+			
+			ctx.lineWidth=4;
+			strokeColor[i % 3] -= 51; // unique frame color for each face
+			var color = "rgb("+strokeColor[0]+","+strokeColor[1]+"," +strokeColor[2]+")";
+			ctx.strokeStyle = color;
+			
+			ctx.strokeRect(canvas.width * box.Left, canvas.height * box.Top, canvas.width * box.Width, canvas.height * box.Height);
+			
+			// iterate through emotions
+			var emotions = '<ul>Emotions: ';
+			$.each( face.Emotions, function( i, emotion ) {
+				emotions += '<li>' + emotion.Type + " (" + (Math.round(emotion.Confidence * 100) / 100) + "%)</li>";
+			});
+			emotions += '</ul>';
+			
+			// append face attributes to #analysis
+			var line = '<p><div style="display:inline-block;width:12px;height:12px;background:'+color+'"></div> <strong>Face '+ (i + 1) + '</strong> &ndash; ' + face.AgeRange.Low + '-' + face.AgeRange.High + ' yrs old, ' + face.Gender.Value + '</p>' + emotions;
+			$('#analysis').append(line);
+		}
+	});
+}
